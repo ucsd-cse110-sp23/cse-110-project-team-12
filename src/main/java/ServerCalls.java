2755b0bfc7cc2691ea6420f16c408373899972f1
Shawn Goh Bo Yaw <74935394+Shawn-Goh@users.noreply.github.com>
2023-05-25 23:53:21 -0700

    Missed out these files

---
+++ src/main/java/ServerCalls.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ServerCalls {
+    public static final String  URL = "http://localhost:8100/";
+
+    public static void postToServer(){
+      
+        try {
+          URL url = new URL(URL);
+          HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+          conn.setRequestMethod("POST");
+          conn.setDoOutput(true);
+          OutputStreamWriter out = new OutputStreamWriter(
+            conn.getOutputStream()
+          );
+          out.write(QuestionPanel.getQuestion() + "," + QuestionPanel.getAnswer());
+          out.flush();
+          out.close();
+          BufferedReader in = new BufferedReader(
+            new InputStreamReader(conn.getInputStream())
+          );
+          
+          in.close();
+          
+        } catch (Exception ex) {
+          ex.printStackTrace();
+        }
+    
+      }
+
+      public static String getFromServer(String query){
+        try {
+          URL url = new URL(URL + "?=" + query);
+          HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+          conn.setRequestMethod("GET");
+      
+
+          BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+          String response = in.readLine();
+          in.close();
+          
+         return response; 
+      }   
+      catch (Exception ex) {
+          //do nothing
+      }
+        return null; //FIXFIXFIXFIXFIXFIXFIXFIXFIXFIXFIXFIXFIXFIXFIXFIXFIXFIXFIX
+      }
+
+      public static void deleteFromServer(String question){
+          
+        try {
+            String query = question.replace(' ', '+');
+            URL url = new URL(URL + "?=" + query);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("DELETE");
+            BufferedReader in = new BufferedReader(
+              new InputStreamReader(conn.getInputStream())
+            );
+            in.readLine();
+            in.close();
+          
+          } catch (Exception ex) {
+            ex.printStackTrace();
+          
+          }
+      
+      }
+}
